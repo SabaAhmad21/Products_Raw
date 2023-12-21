0fefbb491f29417750173504c4e4b18a9ef3e7e5
Saba Ahmad <sabaahmad821@gmail.com>
2023-12-21 17:12:31 +0500

    Start

---
+++ RawMaterial/RawMaterial/Controllers/MaterialController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RawMaterial.Controllers
+{
+    public class MaterialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Materials()
+        {
+            return View();
+        }
+
+        public IActionResult MaterialCreateVM()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ RawMaterial/RawMaterial/ViewModels/Material.cs
@@ -0,0 +1,32 @@
+﻿namespace RawMaterial.ViewModels
+{
+    public class Material
+    {
+        public int Id {  get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Unit { get; set; }
+
+        public string Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+
+    public class MaterialCreateVM
+    {
+        
+
+        public string ProductName { get; set; }
+
+        public string Unit { get; set; }
+
+        public string Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
